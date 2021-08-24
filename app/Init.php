e59bd2e46434492d87e83b973d7387ef19454b11
Michael W. Delaney <michael.delaney@gmail.com>
2021-08-24 14:07:43 -0400

    Update Init.php

--- app/Init.php
+++ app/Init.php
@@ -12,13 +12,5 @@ class Init {
       $mimes['svg'] = 'image/svg+xml';
       return $mimes;
     });
-
-    // Show svg properly in media grid and Feature Images
-    add_action('admin_head', function() {
-      $css = '';
-      $css = 'td.media-icon img[src$=".svg"], img[src$=".svg"].attachment-post-thumbnail { width: 100% !important; height: auto !important; }';
-      echo '<style type="text/css">'.$css.'</style>'; 
-    });
-
   }
 }
